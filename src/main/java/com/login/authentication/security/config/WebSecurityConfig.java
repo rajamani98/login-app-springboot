8b07feb42a2f40a02712f17cdf7383b18674d88e
rajamani98 <67643908+rajamani98@users.noreply.github.com>
2022-01-25 18:06:51 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/login/authentication/security/config/WebSecurityConfig.java
+++ src/main/java/com/login/authentication/security/config/WebSecurityConfig.java
@@ -2,10 +2,13 @@ package com.login.authentication.security.config;
 
 import com.login.authentication.appuser.AppUserService;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @AllArgsConstructor
@@ -25,5 +28,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated().and()
                 .formLogin()
                 .permitAll();
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
